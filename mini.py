b128fbfff5af98fa4b83cbdd4ee97b27e3fda5c4
BlueDice37 <dhairyasharma37@gmail.com>
2021-06-12 22:26:21 +0530

    Update mini.py

--- mini.py
+++ mini.py
@@ -6,7 +6,9 @@ client = discord.Client()
 
 lume = 555955826880413696
 minichannel = 853267392754221077
-dicechannel = 828941473096794142
+bluebelly = 828941473096794142
+dicechannel = 828941629984735252
+rpg-stuff = 848197327935373323
 
 @client.event
 async def on_ready():
@@ -15,7 +17,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     #minion = discord.utils.get(message.guild.roles, name = 'Mini-Berry')
-    channel1 = client.get_channel(828941629984735252)
+    channel1 = client.get_channel(848197327935373323)
     if message.channel != channel1:
         return
     # if minion in message.author.roles:
@@ -34,7 +36,7 @@ async def on_message(message):
                     # continue
     if message.content.startswith('#history'):
         msg = await channel1.history(limit=5).flatten()
-        msg = msg[2]
+        msg = msg[6]
         attach = msg.attachments
         await message.channel.send(f'{attach}')
 
