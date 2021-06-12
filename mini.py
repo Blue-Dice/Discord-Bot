f231bca36c3fa03f6eb8f61840daf04fab50ebeb
BlueDice37 <dhairyasharma37@gmail.com>
2021-06-12 20:33:31 +0530

    Update mini.py

--- mini.py
+++ mini.py
@@ -30,7 +30,7 @@ async def on_message(message):
             msg = await channel1.history(limit=2).flatten()
             msg = msg[1]
             embeds = msg.embeds
-            if 'Guild successfully upgraded!' in embeds.description:
-                await message.channel.send('successful upgrade')
+            for embed in embeds:
+                await message.channel.send(f'{embed.to_dict()}')
 
 client.run(os.getenv('TOKEN'))
