a2485b377f0f96f5a2736fd92b8879ca550ebd1d
BlueDice37 <dhairyasharma37@gmail.com>
2021-06-13 12:47:18 +0530

    Update mini.py

--- mini.py
+++ mini.py
@@ -49,13 +49,12 @@ async def on_message(message):
     minion = discord.utils.get(message.guild.roles, name = 'Mini-Berry')
     jailer = discord.utils.get(message.guild.roles, name = 'jail-check')
     channel1 = client.get_channel(minichannel)
-    default_time = 7200
     if message.channel != channel1:
         return
     if minion in message.author.roles:
         if message.content.startswith('$upgrade'):
             while(1):
-                time_word = '2h 0m 0s'
+                default_time = 7200
                 for x in range(2):
                     await message.channel.send('rpg guild upgrade')
                     def check1(message):
@@ -93,24 +92,19 @@ async def on_message(message):
                     continue
         elif message.content.startswith('$raid'):
             while(1):
-                time_word = '2h 0m 0s'
+                default_time = 7200
                 for x in range(2):
                     await message.channel.send('rpg guild raid')
                     def check3(message):
-                        if message.author.id == lume:
-                            embeds = message.embeds
-                            for embed in embeds:
-                                embed_dict2 = embed.to_dict()
-                            if 'description' in embed_dict2.keys():
-                                return 'RAIDED' in embed_dict2['description']
-                        if message.author == client.user:
-                            return '#time' in message.content
+                        return message.author == client.user and 'Time:' in message.content
                     try:
                         msg = await client.wait_for('message',timeout=10,check=check3)
-                        if '#time' in msg.content:
-                            msg = msg.content.split('#time ',1)[1]
+                        if 'Time' in msg.content:
+                            msg = msg.content.split('Time: ',1)[1]
+                            time_word = msg
                             default_time = await time(msg)
                         await message.channel.send(f'Next Raid in {time_word}')
+                        break
                     except asyncio.TimeoutError:
                         continue
                 def check4(message):
@@ -143,17 +137,20 @@ async def on_message(message):
         await message.channel.send(f'{minion.mention} Jail Alert')
         await message.channel.send(f'{minion.mention} Jail Alert')
         
-    if message.author == client.user and (message.content == 'rpg guild upgrade' or message.content == 'rpg guild raid'):
+    if message.author == client.user and (message.content == 'rpg guild raid'):
         msg = await client.wait_for('message',check = lambda message: message.author.id == lume)
         embeds = msg.embeds
         for embed in embeds:
-            embed_dict = embed.to_dict()
-        if 'title' in embed_dict.keys():
-            if 'wait at least' in embed_dict['title']:
-                msg = embed_dict['title']
+            embed_dict2 = embed.to_dict()
+        if 'title' in embed_dict2.keys():
+            if 'wait at least' in embed_dict2['title']:
+                msg = embed_dict2['title']
                 msg = msg.split('least ',1)[1]
                 msg = msg.split('...',1)[0]
                 time_word = msg
-                await message.channel.send(f'#time {msg}')
+                await message.channel.send(f'Time: {msg}')
+        elif 'description' in embed_dict2.keys():
+            if 'RAIDED' embed_dict2['description']:
+                await message.channel.send(f'Time: **2h 0m 0s**')
 
 client.run(os.getenv('TOKEN'))
